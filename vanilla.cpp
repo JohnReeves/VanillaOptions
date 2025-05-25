bb5f95bc413edc59579fb04a84acd0b4c51b772d
John Reeves <JohnReeves@users.noreply.github.com>
2025-05-25 10:41:00 +0100

    added constructors, getters and destructor

--- vanilla.cpp
+++ vanilla.cpp
@@ -22,8 +22,54 @@ void VanillaOption::copy(const VanillaOption &other) {
     underlying_asset_price = other.underlying_asset_price;
 }
 
+VanillaOption::VanillaOption() {
+    init();
+}
 
+VanillaOption::VanillaOption(
+    const double &_strike_price,
+    const double &_risk_free_rate,
+    const double &_maturity_time,
+    const double &_underlying_asset_price,
+    const double &_volatility_of_underlying_asset) {
 
+    strike_price = _strike_price;
+    risk_free_rate = _risk_free_rate;
+    maturity_time = _maturity_time;
+    underlying_asset_price = _underlying_asset_price;
+    volatility_of_underlying_asset = _volatility_of_underlying_asset;
+}
+
+VanillaOption::VanillaOption(const VanillaOption &rhs) {
+    copy(rhs);
+}
+
+VanillaOption& VanillaOption::operator=(const VanillaOption &rhs) {
+    if (this != &rhs) {
+        copy(rhs);
+    }
+    return *this;
+}
+
+VanillaOption::~VanillaOption() {
+    // compiler tidy up
+}
+
+double VanillaOption::get_strike_price() const {
+    return strike_price;
+}
+
+double VanillaOption::get_risk_free_rate() const {
+    return risk_free_rate;
+}
+
+double VanillaOption::get_maturity_time() const {
+    return maturity_time;
+}
+
+double VanillaOption::get_underlying_asset_price() const {
+    return underlying_asset_price;
+}
 
 
 #endif VANILLA_CPP
