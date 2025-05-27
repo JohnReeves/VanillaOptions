ad1460426bb65a8f78e93d3489b68712dd77afba
John Reeves <JohnReeves@users.noreply.github.com>
2025-05-27 07:34:09 +0100

    Update vanilla.cpp
    relabelled to use standard equation symbols

--- vanilla.cpp
+++ vanilla.cpp
@@ -9,19 +9,19 @@
 #include <cmath>
 
 void VanillaOption::init() {
-    strike_price = 100.0;
-    risk_free_rate = 0.5;
-    maturity_time = 1.0;
-    underlying_asset_price =100.0;
-    volatility_of_underlying_asset = 0.2;
+    K = 100.0;
+    r = 0.5;
+    T = 1.0;
+    S =100.0;
+    sigma = 0.2;
 }
 
 void VanillaOption::copy(const VanillaOption &other) {
-    strike_price = other.strike_price;
-    risk_free_rate = other.risk_free_rate;
-    maturity_time = other.maturity_time;
-    underlying_asset_price = other.underlying_asset_price;
-    volatility_of_underlying_asset = other.volatility_of_underlying_asset;
+    K = other.K;
+    r = other.r;
+    T = other.T;
+    S = other.S;
+    sigma = other.sigma;
 }
 
 VanillaOption::VanillaOption() {
@@ -35,11 +35,11 @@ VanillaOption::VanillaOption(
     const double &_underlying_asset_price,
     const double &_volatility_of_underlying_asset) {
 
-    strike_price = _strike_price;
-    risk_free_rate = _risk_free_rate;
-    maturity_time = _maturity_time;
-    underlying_asset_price = _underlying_asset_price;
-    volatility_of_underlying_asset = _volatility_of_underlying_asset;
+    K = _strike_price;
+    r = _risk_free_rate;
+    T = _maturity_time;
+    S = _underlying_asset_price;
+    sigma = _volatility_of_underlying_asset;
 }
 
 VanillaOption::VanillaOption(const VanillaOption &rhs) {
@@ -57,24 +57,24 @@ VanillaOption::~VanillaOption() {
     // compiler tidy up
 }
 
-double VanillaOption::get_strike_price() const {
-    return strike_price;
+double VanillaOption::getK() const {
+    return K;
 }
 
-double VanillaOption::get_risk_free_rate() const {
-    return risk_free_rate;
+double VanillaOption::getT() const {
+    return r;
 }
 
-double VanillaOption::get_maturity_time() const {
-    return maturity_time;
+double VanillaOption::getr() const {
+    return T;
 }
 
-double VanillaOption::get_underlying_asset_price() const {
-    return underlying_asset_price;
+double VanillaOption::getS() const {
+    return S;
 }
 
-double VanillaOption::get_volatility_asset_price() const {
-    return volatility_of_underlying_asset;
+double VanillaOption::getSigma() const {
+    return sigma;
 }
 
 double normalCDF(double const x) {
@@ -82,17 +82,16 @@ double normalCDF(double const x) {
 }
 
 double VanillaOption::calc_call_price() const {
-    double const sigma_sqrt_T = volatility_of_underlying_asset * sqrt( maturity_time );
-    double const d1 = ( ( log( underlying_asset_price / strike_price ) )
-                    + ( risk_free_rate + 0.5 * pow( volatility_of_underlying_asset, 2.0 ) ) * maturity_time )
+    double const sigma_sqrt_T = sigma * sqrt( T );
+    double const d1 = ( ( log( S / K ) )
+                    + ( r + 0.5 * pow( sigma, 2.0 ) ) * T )
                     / sigma_sqrt_T;
     double const d2 = d1 - sigma_sqrt_T;
-    double const call = strike_price * normalCDF(d1)
-                      - strike_price * exp(risk_free_rate * maturity_time)
+    double const call = K * normalCDF(d1)
+                      - K * exp(r * T)
                       * normalCDF(d2);
     return call;
 }
 
+
 #endif VANILLA_CPP
-
-
